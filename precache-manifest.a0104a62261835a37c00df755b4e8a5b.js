dc4c237389a76b56aebc437f828785a24fd012f9
Johann.Rajakarunanayake <jrajakarunanayake@gmail.com>
2020-09-04 13:14:29 +0100

    Updates

---
+++ precache-manifest.a0104a62261835a37c00df755b4e8a5b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e031543f185aa82e2a5e0f868837113b",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "8c020042a7b28988d819",
+    "url": "/react_test/static/css/main.ad732be0.chunk.css"
+  },
+  {
+    "revision": "ff7eeadc1aa54b60b656",
+    "url": "/react_test/static/js/2.a48a5205.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_test/static/js/2.a48a5205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c020042a7b28988d819",
+    "url": "/react_test/static/js/main.09f06645.chunk.js"
+  },
+  {
+    "revision": "12295fbc9eda445b96c6",
+    "url": "/react_test/static/js/runtime-main.c946dd09.js"
+  },
+  {
+    "revision": "605546898b9afe3ab1095189d7cf4eaa",
+    "url": "/react_test/static/media/dead_men.60554689.png"
+  },
+  {
+    "revision": "6c5b34f7bb966630dd3239f96ed552e4",
+    "url": "/react_test/static/media/folkesange.6c5b34f7.jpg"
+  },
+  {
+    "revision": "18d0b0ca36b958cffee3bc123a595e69",
+    "url": "/react_test/static/media/log.18d0b0ca.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "75f15c38aad7803facc3355ab3021632",
+    "url": "/react_test/static/media/splid.75f15c38.jpg"
+  },
+  {
+    "revision": "498f6c926ba34eb0cca825d2f6bdef83",
+    "url": "/react_test/static/media/underneath.498f6c92.jpg"
+  }
+]);
